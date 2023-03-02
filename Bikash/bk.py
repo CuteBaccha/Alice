9b465a2c6e95b96e2bd5b65697c8f438b486af45
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-03-02 18:51:16 +0530

    Update bk.py

--- Bikash/bk.py
+++ Bikash/bk.py
@@ -1,10 +1,10 @@
-# Powered By @BikashHalder @AdityaHalder
+# Powered By @I_LOVE_YOU_MY_HEARTBEET @ID_SELLER00
 
 from typing import Union, List
 from pyrogram import filters
 from Bikash.config import COMMAND_PREFIXES
 
-## Aditya Bikash
+## ALice family
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
